91a393eda6ca923bacdd88a2149bf58d1173f46c
VivekKota <kvivekreddy1111@gmail.com>
2024-03-24 12:41:38 -0700

    first commit

---
+++ src/components/Balance.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+const Balance = () => {
+
+  
+
+
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>$0.00</h1>
+    </>
+  )
+}
+
+export default Balance
